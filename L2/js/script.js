b935597a008ec1af5e27ebfa7b777de18845f5ab
Claudia Gutenberg <ca222ps@student.lnu.se>
2023-04-06 19:21:36 +0200

    update

---
+++ L2/js/script.js
@@ -0,0 +1,11 @@
+// Globala variabler
+
+
+// --------------------------------------------------
+// Initiering av globala variabler och händelsehanterare
+function init() {
+	
+} // Slut init
+window.onload = init; // Se till att init aktiveras då sidan är inladdad
+// --------------------------------------------------
+
